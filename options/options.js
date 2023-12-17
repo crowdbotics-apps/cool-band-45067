b141f062eb711978f1186cae5cbb1ea657bbdd1b
Crowdbotics <team@crowdbotics.com>
2023-12-17 22:04:00 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cool-band-45067",
+  url: "https://cool-band-45067.botics.co",
+  api: "https://cool-band-45067.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
